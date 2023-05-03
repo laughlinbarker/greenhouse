f839316e05fdd52eb209235979672af8269e3fe7
Laughlin Barker <laughlinbarker@gmail.com>
2023-05-02 21:27:37 -0800

    recv and get into JSON

--- server_side_sw/listener.py
+++ server_side_sw/listener.py
@@ -1,11 +1,19 @@
+import json
 import time
+from datetime import datetime
 import zmq
 
 context = zmq.Context()
 socket = context.socket(zmq.PULL)
-socket.bind("tcp://*:1234")
+socket.bind("tcp://*:7753")
 
 while True:
     #  Wait for next request from client
     message = socket.recv()
-    print("Received request: %s" % message)
+    current_datetime = datetime.now()
+    current_date_time = current_datetime.strftime("%m/%d/%Y, %H:%M:%S")
+    data_dict = json.loads(message.decode('ascii'))
+    print("Recieved: %s" % current_date_time)
+    print(data_dict)
+    print('\n')
+    #print("Received request: %s" % message)
